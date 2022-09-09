6fad74df35b7237d91ef109c469721b78edf4868
maurice4400 <mauchy164@yahoo.com>
2022-09-09 10:36:20 -0700

    5-print

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>                                                             
+/**                                                                              *
+ *  * multiline comment                                                                                                            
+ *   * this program prints multilingual comment                                                                                      
+ *
+ * 
+ */                                                                                                                              
+
+int main(void)                                                                                                                   
+
+{                                                                                 printf("with proper grammar, but the outcome is a piece of art,\n");            return(0);
+} 
