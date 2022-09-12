0696e25cf2784932b157f0c661f05646a8c3b8c3
maurice4400 <mauchy164@yahoo.com>
2022-09-12 02:18:23 -0700

    commit 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,11 +7,11 @@
  *                                                  
  *
  * Description: using the main function          
- * this program prints"programming is like building a multilingual puzzle"
+ * this program prints programming is like building a multilingual puzzle
  * Return: 0
- */                                     
-
-int main(void)                                                                                                                   {                                                                                
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return(0);
+ */                               
+int main(void)            
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return(0);
 }
