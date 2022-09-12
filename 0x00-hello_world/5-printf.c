0bbc65765c56432e879a3ea7fa740d7d187dd8a4
maurice4400 <mauchy164@yahoo.com>
2022-09-12 02:06:32 -0700

    5-printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,17 @@
 #include <stdio.h>                                                             
-/**                                                                              *
- *  * multiline comment                                                                                                            
- *   * this program prints multilingual comment                                                                                      
+/**                                               
  *
- * 
- */                                                                                                                              
+ *  
+ * main - print the string in the put function    
+ *
+ *                                                  
+ *
+ * Description: using the main function          
+ * this program prints"programming is like building a multilingual puzzle"
+ * Return: 0
+ */                                     
 
-int main(void)                                                                                                                   
-
-{                                                                                 printf("with proper grammar, but the outcome is a piece of art,\n");            return(0);
-} 
+int main(void)                                                                                                                   {                                                                                
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return(0);
+}
