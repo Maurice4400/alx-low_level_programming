335892f11c65165946feee29d53fe1d77d7e109a
maurice4400 <mauchy164@yahoo.com>
2022-09-12 08:29:42 -0700

    commit positive

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -18,16 +18,16 @@ int main(void)
 	int n;
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n > 0){
+	if (n > 0)
 		printf("%d is positive\n",n);
-	}
-	elseif (n<0){
+	
+	else if (n==0)
 		printf("%d is negative\n",n);
-	}
+	
 	else
-	{
+
 		print("%d is zero\n",n);
-	}
+	
 	return (0);
 
 }
