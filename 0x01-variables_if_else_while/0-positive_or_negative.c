bc78fc19f0f96534dc1b48b6724666afda1a3315
maurice4400 <mauchy164@yahoo.com>
2022-09-12 08:50:17 -0700

    variable else while

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+/* more headers goes there */
+/*betty style doc for function main goes there */
 
 /**
  *
- *main - assign a random number to the variable n each time it is executed
  *
- *if the number is greater than 0: is positive, if the number is 0: is zero
+ *
+ *main - entry point
  *
  *if the number is less than 0: is negative, followed by a new line
  *
- *Return: 0
+ *Return: Always 0 (success)
  *
  */
 int main(void)
@@ -21,7 +23,7 @@ int main(void)
 	if (n > 0)
 		printf("%d is positive\n",n);
 	
-	else if (n==0)
+	else if (n == 0)
 		printf("%d is negative\n",n);
 	
 	else
