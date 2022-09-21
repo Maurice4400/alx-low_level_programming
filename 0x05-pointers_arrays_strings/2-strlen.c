dcdbf9bd0fef682b529fd008ee437c6078e5d5b3
maurice4400 <mauchy164@yahoo.com>
2022-09-21 05:01:41 -0700

    commit2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,24 +1,21 @@
-#include "holberton.h"
+#include "main.h"
 #include <stdio.h>
 
 /**
- *
- *_strlen - a function that returns the length of a string
- *
- *@s: char input
- *
- *Return: length of the input string
- *
+ * _strlen - a function that returns the length of a string
+ * @s: char input
+ * Return: length of the input string
  */
-
 int _strlen(char *s)
-
 {
-	int i = 1, sum = 0;
-	char pl = s[0];
-	
+	int i;
+	int sum;
+	char pl;
+
+	i = 1;
+	sum = 0;
+	pl = s[0];
 	while (pl != '\0')
-	
 	{
 		sum++;
 		pl = s[i++];
